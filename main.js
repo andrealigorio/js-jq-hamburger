c16c00dccfa91070d875d47b63046345024ef99d
Andrea Ligorio <andrea85kro@gmail.com>
2021-03-04 15:03:01 +0100

    Esercizio completato

---
+++ main.js
@@ -0,0 +1,11 @@
+$('.header-right > a').click(
+    function() {
+        $('.hamburger-menu').fadeIn();
+    }
+);
+
+$('.close').click(
+    function() {
+        $('.hamburger-menu').fadeOut();
+    }
+);
